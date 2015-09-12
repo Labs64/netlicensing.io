4462c055fd0410abe09714e1b958db374b9be8b7
R.Brown <ready.brown@hotmail.de>
2015-09-12 08:23:21 +0100

    - reformat code

--- js/changeURL.js
+++ js/changeURL.js
@@ -7,13 +7,14 @@ $("a#load-features").on("click", function () {
 
     if (linkText === "SHOW MORE FEATURES") {
         linkText = "Hide";
-        url="/features-all";
+        url = "/features-all";
         $content.switchClass("hideContent", "showContent", 1500);
     } else {
         linkText = "Show more features";
-        url="/features"
+        url = "/features"
         $content.switchClass("showContent", "hideContent", 1500);
-    };
+    }
+    ;
 
     $this.text(linkText);
 
@@ -23,49 +24,3 @@ $("a#load-features").on("click", function () {
 
     return false;
 });
-
-/*$(document).ready(function () {
-    $('a#load-features').click(function () {
-        var url = $(this).attr('href');
-        var linkText = $(this).text().toUpperCase();
-
-        if (linkText === "SHOW MORE FEATURES") {
-            linkText = "Hide";
-            $(this).text(linkText);
-            $('#showContent').addClass("hideContent");
-            $.ajax({
-                url: url + '?ajax=1',
-                success: function (data) {
-                    $('#showContent').html(data);
-                    $('#showContent').switchClass("hideContent", "showContent", 1500);
-                }
-            });
-        } else {
-            linkText = "Show more features";
-            $(this).text(linkText);
-            url = "/features";
-            $('#showContent').switchClass("showContent", "hideContent", 1500);
-            $.ajax({
-                url: url + '?ajax=1',
-                success: function (data) {
-                    $('#showContent').html('');
-                }
-            });
-        }
-
-        if (url != window.location) {
-            window.history.pushState(null, null, url);
-        }
-
-        return false;
-    });
-
-    $(window).bind('popstate', function () {
-        $.ajax({
-            url: "/features",
-            success: function (data) {
-                $('#showContent').html('');
-            }
-        });
-    });
-});*/
